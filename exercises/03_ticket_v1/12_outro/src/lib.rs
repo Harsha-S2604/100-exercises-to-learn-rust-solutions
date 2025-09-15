f44a37dfdb45d5dcb07ab3fccf899a842849ba0f
Harsha-S2604 <harsha91148@outlook.com>
2025-09-15 07:02:41 +0000

    ./../../../../scripts/git_push.sh

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -1,5 +1,64 @@
 // TODO: Define a new `Order` type.
 //   It should keep track of three pieces of information: `product_name`, `quantity`, and `unit_price`.
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Self {
+        if product_name.is_empty() {
+            panic!("Product name cannot be empty");
+        }
+        
+        if product_name.len() > 300 {
+            panic!("Procut name cannot be larger than 300 bytes");
+        }
+
+        if quantity <= 0 {
+            panic!("Quantity cannot be zero");
+        }
+
+        if unit_price <= 0 {
+            panic!("Unit price cannot be zero");
+        }
+
+        Self {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    pub fn total(&self) -> u32 {
+        self.quantity * self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, product_name: String) {
+        self.product_name = product_name;
+    }
+
+    pub fn set_quantity(&mut self, quantity: u32) {
+        self.quantity = quantity;
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: u32) {
+       self.unit_price = unit_price;
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+}
 //   The product name can't be empty and it can't be longer than 300 bytes.
 //   The quantity must be strictly greater than zero.
 //   The unit price is in cents and must be strictly greater than zero.
